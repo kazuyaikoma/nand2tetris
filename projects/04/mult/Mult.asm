4c953ec2f50b238613b3aa588a7c5ac50b809bc4
nixii <nixii0986@gmail.com>
2022-07-21 20:07:29 +0900

    :+1: implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,39 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@sum  // 変数宣言
+// e.g. sumのアドレスが1212
+M=0   // sum = 0
+// e.g. M[1212] = 0
+
+// R0
+@0    // 定数宣言
+D=M   // データレジスタに0を代入(D=Memory[0])
+@i    // 変数宣言
+M=D   // i = Memory[0]
+
+(LOOP)
+  @i
+  D=M // D=M[i] => D=i
+  @END
+  D;JEQ
+
+// R1
+  @1
+  D=M // D=M[1]
+  @sum
+  M=D+M // sum=sum+i
+  @i
+  M=M-1 // i=i-1
+  @LOOP
+  0;JMP // LOOPへ移動
+(END)
+
+@sum
+D=M
+
+// R2
+@2
+M=D // R2 = @sum
+@END
+0;JMP
